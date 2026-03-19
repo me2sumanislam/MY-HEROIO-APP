e78d5e4c9e6a650249c94f67b629e279fb38973a
me2sumanislam <me2sumanislam247@gmail.com>
2026-03-19 20:01:40 +0600

    add bannerSection done with figma

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,40 @@
- import React from 'react'
- 
- const Home = () => {
-   return (
-     <div>
-       <h2>ami home</h2>
-     </div>
-   )
- }
- 
- export default Home
- 
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="flex flex-col items-center text-center px-4 py-12 bg-gray-100">
+        <h1 className="text-6xl md:text-7xl font-bold text-[#1e293b] leading-tight">
+          We Build
+        </h1>
+        <h2 className="text-5xl md:text-7xl font-bold mb-6">
+          <span className="text-[#8b5cf6]">Productive</span>{" "}
+          <span className="text-[#1e293b]">Apps</span>
+        </h2>
+        <p className="max-w-2xl text-gray-500 text-lg leading-relaxed mb-10">
+          At HERO.IO, we craft innovative apps designed to make everyday life
+          simpler, smarter, and more exciting. Our goal is to turn your ideas
+          into digital experiences that truly make an impact.
+        </p>
+
+        <div className="flex flex-wrap justify-center gap-4 mb-10">
+          <button className="flex items-center gap-2 px-6 py-3 border border-gray-200 rounded-xl hover:bg-gray-50 shadow-sm transition-all">
+            <img
+              src="/src/assets/playstore.png"
+              alt="googleplay"
+              className="h-6"
+            />
+            <span className="font-semibold text-gray-700">Google Play</span>
+          </button>
+
+          <button className="flex items-center gap-2 px-6 py-3 border border-gray-200 rounded-xl hover:bg-gray-50 shadow-sm transition-all">
+            <img src="/src/assets/applelogo.png" alt="AS" className="h-6" />
+            <span className="font-semibold text-gray-700">App Store</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
