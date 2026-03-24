19f12dbf4f492fc2db346babaf5f74f761a19a63
me2sumanislam <me2sumanislam247@gmail.com>
2026-03-24 22:14:42 +0600

    add apps not found and page not found

--- src/Pages/ErrorPage/Error.jsx
+++ src/Pages/ErrorPage/Error.jsx
@@ -1,12 +1,23 @@
- import React from 'react'
- 
- const Error = () => {
-   return (
-     <div>
-       <h2>Page not found</h2>
-     </div>
-   )
- }
- 
- export default Error
- 
+import React from "react";
+import { Link } from "react-router-dom"; 
+
+const Error = () => {
+  return (
+    <div className="col-span-full flex flex-col items-center justify-center text-center py-20 text-gray-400 min-h-screen">
+      <img
+        src="/src/assets/error-404.png"
+        alt="404 Error"
+        className="w-80 h-auto mb-8 mx-auto"
+      />
+
+      <Link
+        to="/"
+        className="btn bg-[#8b5cf6] hover:bg-[#7c3aed] text-white px-10 py-3 rounded-xl font-bold transition-all active:scale-95 shadow-lg no-underline"
+      >
+         Go Back !
+      </Link>
+    </div>
+  );
+};
+
+export default Error;
